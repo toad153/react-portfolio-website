5c7c66cff964ade7cd41fa2776a6ad37654354ed
toad153 <76248516+toad153@users.noreply.github.com>
2023-08-13 23:11:59 +0530

    updated projects

--- src/components/WorkCardData.js
+++ src/components/WorkCardData.js
@@ -1,6 +1,8 @@
 import pro1 from "../assets/weatherApp-react.png"
-import pro2 from "../assets/blogwebsite.jpg"
-import pro3 from "../assets/todolist-v1.jpg"
+import pro2 from "../assets/weather_prediction_model.png"
+import pro3 from "../assets/Facial_recog_attendance_system.png"
+import pro4 from "../assets/todolist-v1.jpg"
+import pro5 from "../assets/blogwebsite.jpg"
 
 
 const ProjectCardData = [
@@ -14,19 +16,36 @@ const ProjectCardData = [
 
     {
         imgsrc: pro2,
+        title: "Weather Prediction Model",
+        text: "A weather prediction model built in Python using Ridge Regression using historical meteorological data, as well as features such as temperature, humidity, and pressure. Ridge Regression was used to improve forecast accuracy while dealing with multicollinearity.",
+        view: "https://github.com/toad153/weather-prediction-model",
+        source: "https://github.com/toad153/weather-prediction-model"
+    },
+
+    {
+        imgsrc: pro3,
+        title: "Facial Recognition based Attendance System",
+        text: "Using OpenCV and dlib, I created a Python Facial Recognition Attendance System. For efficient and reliable record-keeping, detect faces, match them with a model, and log attendance.",
+        view: "https://github.com/toad153/facial-recognition-based-attendance-system",
+        source: "https://github.com/toad153/facial-recognition-based-attendance-system"
+    },
+    {
+        imgsrc: pro4,
+        title: "ToDo List",
+        text: "Powerful todo list website leveraging Node.js, Express.js, MongoDB, and EJS templates. Seamlessly manage tasks, set reminders, and track progress with a user-friendly interface for efficient organization and productivity.",
+        view: "https://todolist-v1-production-9fa.up.railway.app/",
+        source: "https://github.com/toad153/todolist-v1"
+    },
+
+    {
+        imgsrc: pro5,
         title: "Blog Website",
         text: "Dynamic blog website powered by Node.js, Express.js, and MongoDB. Seamlessly integrates EJS templating for easy content management. Explore engaging articles, share insights, and interact with a responsive and interactive interface.",
         view: "https://blogpost-website-production.up.railway.app/",
         source: "https://github.com/toad153/blogpost-website"
     },
 
-    {
-        imgsrc: pro3,
-        title: "ToDo List",
-        text: "Powerful todo list website leveraging Node.js, Express.js, MongoDB, and EJS templates. Seamlessly manage tasks, set reminders, and track progress with a user-friendly interface for efficient organization and productivity.",
-        view: "https://todolist-v1-production-9fa.up.railway.app/",
-        source: "https://github.com/toad153/todolist-v1"
-    }
+
 ]
 
 export default ProjectCardData
